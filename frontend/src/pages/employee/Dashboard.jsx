72a8c091f8af09c2e9bd26840d34fbb0f35798f8
Mayank Dudhatra <mayankdudhatracg@gmail.com>
2026-01-03 15:16:11 +0530

    Remove One Import

--- frontend/src/pages/employee/Dashboard.jsx
+++ frontend/src/pages/employee/Dashboard.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import Sidebar from "../../components/Sidebar";
-import { useAuth } from "../../context/AuthContext"
 import { FiClock, FiCheckCircle, FiCalendar, FiAlertCircle } from "react-icons/fi";
 import {
   getTodayAttendance,
